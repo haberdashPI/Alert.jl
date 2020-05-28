b6d44b7895a0238bcf6b527981f1b0534417ee1a
David Little <david.frank.little@gmail.com>
2020-05-28 11:30:45 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Alert
+
+Alert provides a cross-platform means of displaying a notification to the user
+in Julia. It should work on MacOS, Windows 10 or Linux. This is handy for
+long-running scripts; just add `alert()` to the end of the script, and go work
+on something else until you see the notification. To use it just run `alert`,
+like so.
+
+```julia
+
+using Alert
+
+for i in 1:10_000
+    long_running_function()
+end
+
+alert("Your julia script is finished!")
+```
+
+Before using `alert()` at the end of a long-running script, it would be good to
+test that it actually works on your system: e.g. some linux distros may not have
+an appropriate program installed to display the notification. If it doesn't
+work, just read the error message that is displayed to see what program you need
+to install.
--- src/Alert.jl
+++ src/Alert.jl
@@ -1,5 +1,51 @@
 module Alert
 
-# Write your package code here.
+@static if Sys.iswindows()
+    using PyCall
+    const wintoast = PyNULL()
+    function __init__()
+        copy!(wintoast, pyimport_conda("win10toast","win10toast","conda-forge"))
+    end
+end
+
+export alert
+
+"""
+
+    alert(message="Done!")
+
+Display a cross-platform notification.
+
+On MacOS, displays a notification window. On linux, tries to use notify-send,
+zenity, kdialog or xmessage, in that order. On windows, uses a toast
+notification.
+
+Other platforms are not yet supported.
+
+"""
+function alert(message="Done!")
+    @static if Sys.isapple()
+        run(`osascript -e 'display notification "'$message'" with title "Julia"'`)
+    elseif Sys.islinux()
+        if !isempty(Sys.which("notify-send"))
+            run(`notify-send $message`)
+        elseif !isempty(Sys.which("zenity"))
+            run(pipeline(`echo $message`,`zenity --notification --listen`))
+        elseif !isempty(Sys.which("kdialog"))
+            run(`kdialog --title "Julia" --passivepopup $message 10`,wait=false)
+        elseif !isempty(Sys.which("xmessage"))
+            run(`xmessage $message`)
+        else
+            @info "Trying to send message: $message."
+            @error("No program for displaying notifications available, install",
+                   " 'notify-send', 'zenity', 'kdialog' or 'xmessage'.")
+        end
+    elseif Sys.iswindows()
+        wintoast.ToastNotifier().show_toast("Julia",message)
+    else
+        @info "Trying to send message: $message."
+        @error "Unsupported operating system."
+    end
+end
 
 end
