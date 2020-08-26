8c0571ea899ff87959375a84056c7f4900335f7e
David Little <david.frank.little@gmail.com>
2020-08-26 14:17:42 -0400

    chore: reorganized code

--- src/Alert.jl
+++ src/Alert.jl
@@ -4,8 +4,31 @@ using Dates
 using Printf
 using REPL
 
-const default_backend = Ref{Function}()
-const alert_backend = Ref{Function}()
+export alertREPL, alert, @alert, set_alert_backend!
+
+function __init__()
+    init_alert_backends()
+    init_alertREPL()
+end
+
+# `alert` implementation
+# =================================================================
+
+"""
+
+    alert(message="Done!")
+
+Display a cross-platform notification.
+
+On MacOS, displays a notification window. On linux, tries to use notify-send,
+zenity, kdialog or xmessage, in that order. On Windows or WSL2, uses a toast
+notification.
+
+Other platforms are not yet supported.
+
+"""
+alert(message="Done!") = alert_backend[](message)
+
 """
 
     set_alert_backend!(fn)
@@ -20,14 +43,12 @@ If you wish to revert to the default backend, call this mehtod with no arguments
 set_alert_backend!(fn::Function) = alert_backend[] = fn
 set_alert_backend!() = alert_backend[] = default_backend[]
 
-export alertREPL, alert, @alert
+const default_backend = Ref{Function}()
+const alert_backend = Ref{Function}()
 
-function __init__()
-    if VERSION >= v"1.5"
-        pushfirst!(REPL.repl_ast_transforms, with_repl_alert)
-    end
-
-    default_backend[] = if Sys.isapple()
+function init_alert_backends()
+    # select an appropriate implementation for sending alert notifications
+    default_backend[] = @static if Sys.isapple()
         message -> run(`osascript -e 'display notification "'$message'" with title "Julia"'`)
     elseif Sys.islinux()
         if iswsl()
@@ -55,35 +76,70 @@ function __init__()
     alert_backend[] = default_backend[]
 end
 
-const repl_alert_options = Ref((Inf,"Done!"))
-function with_repl_alert(ex)
-    if !isinf(repl_alert_options[][1])
-        Expr(:toplevel, quote
-            @alert $(repl_alert_options[][1]) $(repl_alert_options[][2]) $ex
-        end)
-    else
-        ex
-    end
-end
+# Windows specific methods: `win_toast` and `iswsl`
+# -----------------------------------------------------------------
 
-"""
-    alertREPL([duration=2.0], [message="Done!"])
+@static if Sys.iswindows()
 
-Wraps all code passed to the REPL in an `@alert` macro with the given arguments. Hence,
-if anything you run in the REPL takes longer than `duration` seconds, an alert notification
-will be displayed. You can set the duration to `Inf` to turn off the notification.
-"""
-function alertREPL(args...)
-    repl_alert_options[] = __at_alert_options__(args...)
-    dur = repl_alert_options[][1]
-    if !isinf(dur)
-        secs = @sprintf("%1.2f s", repl_alert_options[][1])
-        @info "Alert will be sent if REPL line takes longer than $secs to complete. See "*
-            "documentation for `Alert.alertREPL`"
-    else
-        @info "REPL alerts have been turned off."
+    # determine if the current system is Linux under WSL
+    function iswsl()
+        try
+            v = read("/proc/version", String)
+            return occursin(r"microsoft",v)
+        catch
+            return false
+        end
     end
-end
+
+    # display windows notification via powershell script
+    function win_toast(content)
+        # format script input
+        io = IOBuffer()
+        show(io, content)
+        disp_content = String(take!(io))
+
+        # create a powershell script
+        posh_script = """
+        \$ErrorActionPreference = "Stop"
+
+        \$notificationTitle = $disp_content
+
+        [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > \$null
+        \$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText01)
+
+        #Convert to .NET type for XML manipuration
+        \$toastXml = [xml] \$template.GetXml()
+        \$toastXml.GetElementsByTagName("text").AppendChild(\$toastXml.CreateTextNode(\$notificationTitle)) > \$null
+
+        #Convert back to WinRT type
+        \$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+        \$xml.LoadXml(\$toastXml.OuterXml)
+
+        \$toast = [Windows.UI.Notifications.ToastNotification]::new(\$xml)
+        \$toast.Tag = "PowerShell"
+        \$toast.Group = "PowerShell"
+        \$toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(5)
+        #\$toast.SuppressPopup = \$true
+
+        \$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+        \$notifier.Show(\$toast);
+        """
+
+        # encode as Base64 of Unicode-16
+        io = IOBuffer()
+        encode = Base64EncodePipe(io)
+        write(encode, transcode(UInt16, posh_script))
+        close(encode)
+        str = String(take!(io))
+
+        # send to powershell (works on windows and WSL2)
+        run(`powershell.exe -Enc $str`)
+    end
+
+end # if Sys.iswindows()
+
+# `@alert` implementation
+# =================================================================
 
 """
     @alert [duration] [message] begin
@@ -118,75 +174,40 @@ __at_alert_options__(num::Number) = convert(Float64,num), "Done!"
 __at_alert_options__(str::AbstractString,num::Number) = convert(Float64,num), str
 __at_alert_options__(num::Number,str::AbstractString) = convert(Float64,num), str
 
-# determine if the current system is Linux under WSL
-function iswsl()
-    try
-        v = read("/proc/version", String)
-        return occursin(r"microsoft",v)
-    catch
-        return false
+"""
+    alertREPL([duration=2.0], [message="Done!"])
+
+Wraps all code passed to the REPL in an `@alert` macro with the given arguments. Hence,
+if anything you run in the REPL takes longer than `duration` seconds, an alert notification
+will be displayed. You can set the duration to `Inf` to turn off the notification.
+"""
+function alertREPL(args...)
+    repl_alert_options[] = __at_alert_options__(args...)
+    dur = repl_alert_options[][1]
+    if !isinf(dur)
+        secs = @sprintf("%1.2f s", repl_alert_options[][1])
+        @info "Alert will be sent if REPL line takes longer than $secs to complete. See "*
+            "documentation for `Alert.alertREPL`"
+    else
+        @info "REPL alerts have been turned off."
     end
 end
 
-"""
-
-    alert(message="Done!")
-
-Display a cross-platform notification.
-
-On MacOS, displays a notification window. On linux, tries to use notify-send,
-zenity, kdialog or xmessage, in that order. On Windows or WSL2, uses a toast
-notification.
-
-Other platforms are not yet supported.
-
-"""
-alert(message="Done!") = alert_backend[](message)
-
-function win_toast(content)
-    # format script input
-    io = IOBuffer()
-    show(io, content)
-    disp_content = String(take!(io))
-
-    # create a powershell script
-    posh_script = """
-    \$ErrorActionPreference = "Stop"
-
-    \$notificationTitle = $disp_content
-
-    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > \$null
-    \$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText01)
-
-    #Convert to .NET type for XML manipuration
-    \$toastXml = [xml] \$template.GetXml()
-    \$toastXml.GetElementsByTagName("text").AppendChild(\$toastXml.CreateTextNode(\$notificationTitle)) > \$null
-
-    #Convert back to WinRT type
-    \$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
-    \$xml.LoadXml(\$toastXml.OuterXml)
-
-    \$toast = [Windows.UI.Notifications.ToastNotification]::new(\$xml)
-    \$toast.Tag = "PowerShell"
-    \$toast.Group = "PowerShell"
-    \$toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(5)
-    #\$toast.SuppressPopup = \$true
-
-    \$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
-    \$notifier.Show(\$toast);
-    """
-
-    # encode as Base64 of Unicode-16
-    io = IOBuffer()
-    encode = Base64EncodePipe(io)
-    write(encode, transcode(UInt16, posh_script))
-    close(encode)
-    str = String(take!(io))
-
-    # send to powershell (works on windows and WSL2)
-    run(`powershell.exe -Enc $str`)
+const repl_alert_options = Ref((Inf,"Done!"))
+function with_repl_alert(ex)
+    if !isinf(repl_alert_options[][1])
+        Expr(:toplevel, quote
+            @alert $(repl_alert_options[][1]) $(repl_alert_options[][2]) $ex
+        end)
+    else
+        ex
+    end
 end
 
-
+function init_alertREPL()
+    if VERSION >= v"1.5"
+        pushfirst!(REPL.repl_ast_transforms, with_repl_alert)
+    end
+end
 
 end
