e990a61cf11c18e19f1326b5e90cd9001123e348
Jack Shannon <j.b.shannon@lse.ac.uk>
2021-08-12 22:58:33 +0100

    added apple_backend function

--- src/Alert.jl
+++ src/Alert.jl
@@ -1,6 +1,7 @@
 module Alert
 using Base64
 using Dates
+using Logging
 using Printf
 using REPL
 
@@ -212,6 +213,52 @@ function init_alertREPL()
     if VERSION >= v"1.5"
         pushfirst!(REPL.repl_ast_transforms, with_repl_alert)
     end
+end
+
+"""
+
+    apple_backend(title="Julia", subtitle="", sound="", test=false)
+
+Helper function to define a Script Editor backend for MacOS systems.
+
+# Arguments
+- `title::AbstractString="Julia"`: Notification title
+- `subtitle::AbstractString=""`: Notification subtitle
+- `sound::AbstractString=""`: Notification sound
+- `test::Bool=false`: Display a sample notification when initializing
+
+# Examples
+```jldoctest
+julia> mybackend = Alert.apple_backend(title="𝒥𝓊𝓁𝒾𝒶", subtitle="Alert.jl", sound="Crystal");
+
+julia> Alert.set_alert_backend!(mybackend);
+
+julia> alert("From a fancy backend!")
+```
+"""
+function apple_backend(;
+    title::AbstractString="Julia",
+    subtitle::AbstractString="",
+    sound::AbstractString="",
+    test::Bool=false
+)
+    @static if !Sys.isapple()
+        @warn "You are attempting to set a MacOS backend on a different system!"
+    end
+
+    options = ""
+    options = length(title) > 0 ? options * " with title \"$title\"" : options
+    options = length(subtitle) > 0 ? options * " subtitle \"$subtitle\"" : options
+    options = length(sound) > 0 ? options * " sound name \"$sound\"" : options
+
+    @debug "Current command" cmd=`osascript -e 'display notification "'message'"'$options''`
+
+    if test
+        testmsg = "Your notifications will now look like this!"
+        run(`osascript -e 'display notification "'$testmsg'"'$options''`)
+    end
+
+    return message -> run(`osascript -e 'display notification "'$message'"'$options''`)
 end
 
 end
