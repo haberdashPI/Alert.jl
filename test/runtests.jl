8524212c12e4cce8acabc79514e892ed40d0b482
David Little <david.frank.little@gmail.com>
2025-10-16 20:30:52 -0400

    fix ci error
    Created using spr 1.3.6-beta.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Test
 using Alert
 
 # without a backend, `alert` should error
-if get(ENV,"CI","false") == "true"
-    @test_throws ProcessFailedException alert()
+if get(ENV, "CI", "false") == "true"
+    @test_throws ErrorException alert()
 else
     @test alert() !== nothing
 end
